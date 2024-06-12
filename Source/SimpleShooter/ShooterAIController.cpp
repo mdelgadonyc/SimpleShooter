ba74a409c8a058caa359cbb4ac667f8669678176
Moises Delgado <moises.f.delgado@gmail.com>
2024-06-12 14:36:39 -0700

    Adds a behavior tree for AI functionality

---
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+    if (AIBehavior != nullptr)
+    {
+        RunBehaviorTree(AIBehavior);
+        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+    }
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    // if (LineOfSightTo(PlayerPawn))
+    // {
+    //     SetFocus(PlayerPawn);
+    //     MoveToActor(PlayerPawn, AcceptanceRadius);
+    // }
+    // else
+    // {
+    //     ClearFocus(EAIFocusPriority::Gameplay);
+    //     StopMovement();
+    // }
+}
